622f37a7d2bb01843bbf437e6dfcee0d03a2f409
Montragor <antonio.panariello@web.de>
2026-04-17 10:27:07 +0200

    Konfiguriert CORS

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import FRONTEND_ORIGIN
 from app.routers import lessons, quiz
+
 
 app = FastAPI(title="React Quest API")
 
-app.include_router(lessons.router) 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_ORIGIN]
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(lessons.router)
 app.include_router(quiz.router)
 
 @app.get("/")
 def read_root():
     return {"message": "React Quest API is running."}
-
